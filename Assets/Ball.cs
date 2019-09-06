9b09604da41ea2cbe42ecdb61cf129d0cce1d5df
Andres Aguirre <47033760+aandres44@users.noreply.github.com>
2019-09-06 10:57:59 -0500

    First commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+
+    public float speed;
+    public int score;
+    public Text countText,
+                winText;
+
+    private Rigidbody rb;
+
+    public Transform original;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "";
+        score = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float u = Input.GetAxis("Horizontal");
+        float s = Input.GetAxis("Vertical");
+
+        transform.Translate
+            (
+            s * Time.deltaTime * speed,
+            0,
+            - u * Time.deltaTime * speed,
+            Space.World
+            );
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y == 0.5)
+        {
+            rb.AddForce(transform.up * 5, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            //print("chocamos");
+            score--;
+        }
+
+        if (collision.gameObject.layer == 9)
+        {
+            
+        }
+
+        SetCountText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            transform.position = original.position;
+            score++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + score.ToString();
+        if (score == 10)
+        {
+            winText.text = "Ganaste";
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed * 10);
+        //transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime);
+
+        if (transform.eulerAngles.y == 180)
+        {
+            if (transform.position.z > 14 && speed < 0)
+            {
+                speed *= -1;
+            }
+            else if (transform.position.z < -14 && speed > 0)
+            {
+                speed *= -1;
+            }
+        }
+        else
+        {
+            if (transform.position.z > 14 && speed > 0)
+            {
+                speed *= -1;
+            }
+            else if (transform.position.z < -14 && speed < 0)
+            {
+                speed *= -1;
+            }
+        }
+        
+    }
+}
